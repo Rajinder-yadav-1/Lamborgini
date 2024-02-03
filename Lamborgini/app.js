39b4f0dc98296b709f6bbf9c550ab579d181743e
Rajinder Yadav <rajinderyadav368@gmail.com>
2024-02-03 12:18:03 +0530

    some canges

--- Lamborgini/app.js
+++ Lamborgini/app.js
@@ -1,7 +1,15 @@
 
-function playsound(audioname){
-    let audio = new Audio(audioname);
-    audio.loop = true;
-    audio.play();
-}
-playsound("sound.mp3")
+document.addEventListener('DOMContentLoaded', function() {
+    const rightAudio = document.querySelector(".right-audio");
+
+    const playRightAudio = function () {
+        rightAudio.load();
+        rightAudio.play();
+    };
+
+    // Assuming you want to play the audio on some event, e.g., button click
+    document.querySelector('.container').addEventListener('click', function() {
+        playRightAudio();
+    });
+});
+
